5b1b27047ad80d44b0676ee84fa70a440511f47f
Grmchurch <gardenoftheremnantsministry@gmail.com>
2025-08-18 13:13:15 +0100

    Updated DigitalCheckin.jsx

--- src/pages/Attendance/DigitalCheckin.jsx
+++ src/pages/Attendance/DigitalCheckin.jsx
@@ -20,6 +20,25 @@ const {
   FiMonitor
 } = FiIcons;
 
+const FiSave = (props) => (
+  <svg
+    stroke="currentColor"
+    fill="none"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path>
+    <polyline points="17,21 17,13 7,13 7,21"></polyline>
+    <polyline points="7,3 7,8 15,8"></polyline>
+  </svg>
+);
+
 const FiX = (props) => (
   <svg
     stroke="currentColor"
